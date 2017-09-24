4a1b1d11bca22fd96dfd045d1e6ee0e3dcf7e067
Domnik Mack <domnikmack@gmail.com>
2017-09-24 01:55:30 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,14 @@
 function forLoop(array) {
-  for ( i = 0; i < 25; i++) {
+  for ( var i = 0; i < 25; i++) {
     array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
   }
   return array;
+}
+
+var n;
+function whileLopp (n) {
+  while (n >= 0) {
+    console.log(n--);
+  }
+  return 'done';
 }
