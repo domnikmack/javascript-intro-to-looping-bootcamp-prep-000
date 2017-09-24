5f71224fe26ae546a88bbcd117172bd5b62fcd17
Domnik Mack <domnikmack@gmail.com>
2017-09-24 02:11:51 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -12,3 +12,14 @@ function whileLoop (n) {
   }
   return 'done';
 }
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop (array) {
+  do {
+    array.pop()
+  } while (maybeTrue() && array.length > 0)
+return array;  
+}
